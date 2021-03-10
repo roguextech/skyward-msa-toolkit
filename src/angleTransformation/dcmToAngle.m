67725f790cdba29bd3b06e4824815e121f5f1e4c
giuliopcfc <56389329+giuliopcfc@users.noreply.github.com>
2021-03-10 22:28:28 +0100

    Correction of the singularity detection on dcmToAngle.m

--- src/angleTransformation/dcmToAngle.m
+++ src/angleTransformation/dcmToAngle.m
@@ -1,12 +1,17 @@
 function [yaw, pitch, roll] = dcmToAngle(dcm)
 
 pitch = squeeze(-asin(dcm(1, 3, :)));
-if cos(pitch) ~= 0
-    yaw = squeeze(atan2(dcm(1, 2, :), dcm(1, 1, :)));
-    roll = squeeze(atan2(dcm(2, 3, :), dcm(3, 3, :)));
-else
-    yaw = NaN;
-    roll = NaN;
-    warning('inverse transformation from dcm to Angle not defined for' +...
-        'cos(pitch = 0)');
+yaw = squeeze(atan2(dcm(1, 2, :), dcm(1, 1, :)));
+roll = squeeze(atan2(dcm(2, 3, :), dcm(3, 3, :)));
+
+singularities = abs(cos(pitch)) < 1e-2;
+
+if any(singularities) 
+    
+    warning('Singularity condition for Euler angles is met. (cos(pitch) = 0)');
+    
+    pitch(singularities) = NaN;
+    yaw(singularities) = NaN;
+    roll(singularities) = NaN;
+    
 end
