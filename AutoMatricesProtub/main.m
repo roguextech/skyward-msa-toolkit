018f15a83f1bd8f08b1d21f32cf519cc91b0a543
Filippo Inno <31957447+filippoinno1994@users.noreply.github.com>
2021-03-24 12:04:35 +0100

    comments aligned

--- AutoMatricesProtub/main.m
+++ AutoMatricesProtub/main.m
@@ -14,7 +14,7 @@ Release date: 18/10/2019
 datcom.Mach = 0.05:0.05:1;
 datcom.Alpha = [-22 -15 -10 -7.5 -5 -2.5 -1 -0.5 -0.1 0 0.1 0.5 1 2.5 5 7.5 10 15 22];
 datcom.Beta = [-5 -2.5 -0.1 0 0.1 2.5 5];
-Alt0 = 109;                                     % [m] local altitude
+Alt0 = 109;                                          % [m] local altitude
 datcom.Alt = Alt0 + (0:400:4000);
 
 %% Design Parameters
@@ -24,16 +24,16 @@ datcom.Height = 0.12;
 datcom.shape = 'rect';
 
 %% Fixed Parameters
-vars.xcg = [1.49, 1.33];                        % [m] CG position [full, empty]
-datcom.D = 0.15;                                % [m] rocket diameter
-datcom.Lnose = 0.26;                            % [m] nose length
-Ltotal = 2.495;                                 % [m] rocket length
-datcom.Lcenter = Ltotal - datcom.Lnose;         % [m] Centerbody length
-datcom.Npanel = 3;                              % [m] number of fins
-datcom.Ler = 0.003;                             % [deg] Leading edge radius
-datcom.d = 0;                                   % [m] rocket tip-fin distance
-datcom.zup_raw = 0.0015;                        % [m] fin semi-thickness 
-datcom.Lmaxu_raw = 0.0015;                      % [m] Fraction of chord from leading edge to max thickness
+vars.xcg = [1.49, 1.33];                             % [m] CG position [full, empty]
+datcom.D = 0.15;                                     % [m] rocket diameter
+datcom.Lnose = 0.26;                                 % [m] nose length
+Ltotal = 2.495;                                      % [m] rocket length
+datcom.Lcenter = Ltotal - datcom.Lnose;              % [m] Centerbody length
+datcom.Npanel = 3;                                   % [m] number of fins
+datcom.Ler = 0.003;                                  % [deg] Leading edge radius
+datcom.d = 0;                                        % [m] rocket tip-fin distance
+datcom.zup_raw = 0.0015;                             % [m] fin semi-thickness 
+datcom.Lmaxu_raw = 0.0015;                           % [m] Fraction of chord from leading edge to max thickness
 
 %% Ogive parameters
 datcom.OgType = 'KARMAN';
@@ -41,10 +41,10 @@ datcom.NosePower = 1/2;
 
 %% Protuberance parameters
 datcom.xprot = datcom.Lcenter + datcom.Lnose - 0.85; % axial position 
-datcom.nloc = 3; % number of brakes
-datcom.lprot = 0.005; % brakes thickness
-datcom.wprot = 0.088; % brakes width
-vars.hprot = linspace(0, 0.0387, 3); % brakes length, first entry must be always 0!
+datcom.nloc = 3;                                     % number of brakes
+datcom.lprot = 0.005;                                % brakes thickness
+datcom.wprot = 0.088;                                % brakes width
+vars.hprot = linspace(0, 0.0387, 30);                % brakes length, first entry must be always 0!
 
 
 %% Run 
